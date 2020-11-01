c9e54de440b922b22fd1fb8aa614b3361f99bae0
Yan Liu <yliu3864@uwo.ca>
2020-10-31 21:17:11 -0400

    search fullname

--- students/src/app/profile/profile.component.ts
+++ students/src/app/profile/profile.component.ts
@@ -37,98 +37,110 @@ export class ProfileComponent implements OnInit {
   constructor(private dataService:DataService,private router:Router) { }
 
   ngOnInit() {
-    this.refreshStudents()
-    
-
-  }
-  refreshStudents(){
     this.dataService.getAllStudents().subscribe(
       response=>{
         
-
         this.students=response['students'];
    
-        this.allstudents=this.students
-        
+        this.allstudents=this.students;
       }
     )
+  
   }
+
   getAverage(grades):number{
    
     let sum=0;
+
     for(let i=0;i<grades.length;i++){
+       
       sum+=Number(grades[i]);
+
     }
+    
     return sum/grades.length
   }
+
   search(){ 
     this.searchStudent=this.allstudents
    
     if(this.searchText!=""){
       
       this.searchStudent=this.searchStudent.filter(res=>{
-      
-        return res.firstName.toLocaleLowerCase().startsWith(this.searchText.toLocaleLowerCase())||res.lastName.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase());
+ 
+        let fullName=res.firstName.concat(res.lastName.toString())
+ 
+        return res.firstName.toLocaleLowerCase().startsWith(this.searchText.toLocaleLowerCase())||res.lastName.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase())
+        ||fullName.toLocaleLowerCase().startsWith(this.searchText.toLocaleLowerCase());
         
       });
-      this.students= this.searchStudent
+
+      this.students= this.searchStudent;
+
     }else if(this.searchText ==""){
-      this.ngOnInit();
+      
+      // location.reload();
+
     }
 
   }
+
   getDetail(i){
-    this.tags=[]
-    if(this.detail[i]==true){
-      this.detail[i]=false;
-    }else{
-      this.detail[i]=true;
-    }
-    
+
+    this.tags=[];
+
+    this.detail[i] =!this.detail[i]; 
+
   }
 
 
   addTag(taginput:String,id){
     
-    
-   
-
     this.tags.push(
       taginput
     )
-    this.students[id-1].tags=this.tags;
- 
 
+    this.students[id-1].tags=this.tags;
  
   }
 
-
   searchTag(){ 
-    this.searchStudent=this.allstudents
+
+    this.searchStudent=this.allstudents;
+
     if(this.searchText!=""){
-    this.searchStudent=this.searchStudent.filter(res=>{
+
+      this.searchStudent=this.searchStudent.filter(res=>{
       
       return res.tags!==undefined;
       
     });
+
     this.searchStudent=this.searchStudent.filter(res=>{
+     
       for(let i=0;i<res.tags.length;i++){
-      return res.tags[i].match(this.seTag);
+
+        return res.tags[i].match(this.seTag);
+      
       }
+
     });
    
        
-    this.students= this.searchStudent
+    this.students = this.searchStudent;
    
-  }else if(this.searchText ==""){
-    this.ngOnInit();
+    }else if(this.searchText ==""){
 
   }
-
+  
   }
+
   toggleClass(e) {
+    
     const classList = e.target.classList;
+    
     const classes = e.target.className;
+   
     classes.includes('clicked') ? classList.remove('clicked') : classList.add('clicked');
   }
 }
